674aa0d39cc8ead358054f2481e053cbfb455616
LukasSchagerl <ic21b117@technikum-wien.at>
2022-11-26 12:57:50 +0100

    addded MM_Monitor

---
+++ src/main/java/at/fhtw/maintenancemonitor/MaintenanceMonitorController.java
@@ -0,0 +1,18 @@
+package at.fhtw.maintenancemonitor;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MaintenanceMonitorController {
+
+    public String m = "Everything works as expected";
+
+// show status message http://localhost:8080/api/message
+    @GetMapping("/api/message")
+    public String printMessage(){
+        return m;
+    }
+
+}
