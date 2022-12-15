d02c0428286f819a35c27f06a6b43fb2eebe8d4a
LukasSchagerl <ic21b117@technikum-wien.at>
2022-12-15 21:28:29 +0100

    add testing PrintMessage()

--- src/test/java/at/fhtw/maintenancemonitor/MaintenanceMonitorApplicationTests.java
+++ src/test/java/at/fhtw/maintenancemonitor/MaintenanceMonitorApplicationTests.java
@@ -1,5 +1,6 @@
 package at.fhtw.maintenancemonitor;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -8,6 +9,12 @@ class MaintenanceMonitorApplicationTests {
 
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	public void test_maintenanceMonitiorPrintMessage(){
+		MaintenanceMonitorController monitor = new MaintenanceMonitorController();
+		Assertions.assertEquals("Everything works as expected", monitor.printMessage());
 	}
 
 }
